f70d59f435b2d949553019036996d7a09db828f2
gayatrisathawane <gayatripsathawane@gmail.com>
2023-09-29 09:49:27 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import config from './utility/config.json'
+const FOOTERNAME = "Developed by gayatri";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container ">
+      <h1 className='heading bg-warning'>{config.title}</h1>
+      <h2>{config.desc}</h2>
+
+      <h2> Some Popular Sweet here....😋😋😋😋😋</h2>
+      <div style={{display:"flex" ,flexDirection:config.direction,flex:"wrap"}}>
+     {
+      config.products.map((productdata,index)=>{
+        const {img,name,price}=productdata
+        return(
+      <div className='cardsweet' key={index}>
+            <img src={img} className='primg'/>
+            <h3>{name}</h3>
+            <h4>price :{price}</h4>
+            <button className='btn btn-warning'>Buy Now</button>
+          </div>
+        )
+      })
+     }
+     </div>
+     <div className='bg-warning fs-3 p-4 text-center'>
+     <h2> {FOOTERNAME}</h2>
+     </div>
     </div>
   );
 }
-
 export default App;
